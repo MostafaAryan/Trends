eb0a2e6610e67b813e2c59b2038da6090de98369
moci <arian.m.mail@gmail.com>
2023-08-25 09:02:42 +0330

    - refactor : Removed redundant code.

--- core/common/src/main/java/com/programmerofpersia/trends/common/navigation/TrNavGraph.kt
+++ core/common/src/main/java/com/programmerofpersia/trends/common/navigation/TrNavGraph.kt
@@ -2,11 +2,6 @@ package com.programmerofpersia.trends.common.navigation
 
 
 /* todo: change label to @StringRes */
-abstract class BaseNavGraph(val route: String, val label: String) {
-
-}
-
-/* todo: change label to @StringRes */
 interface TrNavGraph {
     val route: String
     val label: String
