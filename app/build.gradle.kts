da29a73816919058d82d46a2fa5c759de130eba4
moci <arian.m.mail@gmail.com>
2023-08-26 14:28:59 +0330

    - refactor : Remove comments from build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,13 +32,6 @@ android {
             )
         }
     }
-    /*compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }*/
     buildFeatures {
         compose = true
     }
