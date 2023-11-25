f16de20a4c87ed3fc82bca33d51bd58dac11798d
moci <arian.m.mail@gmail.com>
2023-11-25 17:42:58 +0330

    - fix : Improve implementation of try-catch blocks inside suspend functions.

--- core/data/src/main/java/com/programmerofpersia/trends/data/remote/ApiExecutorImpl.kt
+++ core/data/src/main/java/com/programmerofpersia/trends/data/remote/ApiExecutorImpl.kt
@@ -2,6 +2,7 @@ package com.programmerofpersia.trends.data.remote
 
 import com.programmerofpersia.trends.data.remote.model.TrResponse
 import retrofit2.Response
+import kotlin.coroutines.cancellation.CancellationException
 
 class ApiExecutorImpl : ApiExecutor {
 
@@ -17,6 +18,17 @@ class ApiExecutorImpl : ApiExecutor {
                 TrResponse.Error("Trending: Error in api execution!")
 
         } catch (e: Exception) {
+            /*
+            * We should not prevent coroutine's cancellation exception from being thrown.
+            * Therefore, inside a suspend function or a coroutine scope, either check the exception's
+            * type and throw it if it was a cancellationException, or only catch specific exceptions
+            * (e.g.: NumberFormatException, IOException, etc.) and leave other exceptions to be thrown.
+            *
+            * Throwing cancellationException notifies super functions and super coroutine scopes
+            * that the coroutine has been cancelled.
+            * */
+            if(e is CancellationException) throw e
+
             TrResponse.Error("Trending: ${e.message}")
         }
     }
